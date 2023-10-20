d0bfcb076080ccc189d07c517e4af695f1e7e882
AymeRx <aymeric.surre@gmail.com>
2023-10-20 12:12:51 +0200

    Affichage du type des valeur dans le csv

--- main.py
+++ main.py
@@ -33,5 +33,60 @@ for i, serie in enumerate(series_info):
 data_sorted = sorted(data, key=lambda x: int(''.join(filter(str.isdigit, x[0]))))
 
 # Afficher les données triées
-for item in data_sorted:
-    print(item)
+# for item in data_sorted:
+    # print(item)
+
+def simple_type(value):
+    t = type(value).__name__
+    if t == 'str':
+        return 'string'
+    if t == 'int':
+        return 'integer'
+    if t == 'float':
+        return 'float'
+
+
+# Enregistrez les données dans un fichier CSV
+with open('data/files/episodes.csv', 'w', encoding='utf-8') as file:
+    for item in data_sorted:
+        series_name = item[3]
+        episode = int(item[5])
+        season = int(item[4])
+        url_episode = serie.find('a', class_='liens')['href']
+        file.write(f"{series_name};{episode};{season};{url_episode} # {simple_type(series_name)} {simple_type(episode)} {simple_type(season)} {simple_type(url_episode)}\n")
+
+
+def read_episodes(filename):
+    # Ouvrir le fichier en mode lecture
+    with open(filename, 'r', encoding='utf-8') as file:
+        # Lire toutes les lignes
+        lines = file.readlines()
+
+    # Ignorer la première ligne (l'en-tête)
+    lines = lines[1:]
+
+    # Liste pour stocker les épisodes
+    episodes = []
+
+    for line in lines:
+        # Supprimer les caractères d'espacement en début et fin de ligne
+        line = line.strip()
+
+        # Séparer la ligne en utilisant le séparateur ";"
+        parts = line.split(";")
+
+        # Convertir les données aux types appropriés
+        name = parts[0]
+        episode = int(parts[1])
+        season = int(parts[2])
+        url = parts[3]
+
+        # Ajouter le tuple à la liste
+        episodes.append((name, episode, season, url))
+
+    return episodes
+
+# Test de la fonction
+episodes_data = read_episodes('data/files/episodes.csv')
+for episode in episodes_data:
+    print(episode)
