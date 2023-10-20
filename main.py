a943c7fd3467408005c6710e0fd88cdf4b9f6296
AymeRx <aymeric.surre@gmail.com>
2023-10-20 15:30:32 +0200

    Refactor du code sous forme de fonctions

--- main.py
+++ main.py
@@ -3,159 +3,111 @@ from bs4 import BeautifulSoup
 import psycopg2
 import sqlite3
 
+BASE_URL = "https://www.spin-off.fr/"
 
-url = "https://www.spin-off.fr/calendrier_des_series.html"
+def fetch_web_data(url):
+    """Récupère les données du site web."""
+    response = requests.get(url)
+    return BeautifulSoup(response.content, 'html.parser')
 
-response = requests.get(url)
-soup = BeautifulSoup(response.content, 'html.parser')
 
-# Récupérer toutes les dates
-dates_divs = soup.find_all('div', class_='div_jour')
-dates = [date_div.find('div').text.strip() + date_div.contents[1].strip() for date_div in dates_divs]
+def extract_series_info(soup):
+    """Extrait les informations des séries à partir du BeautifulSoup passé en paramètre."""
+    dates_divs = soup.find_all('div', class_='div_jour')
+    dates = [date_div.find('div').text.strip() + date_div.contents[1].strip() for date_div in dates_divs]
+    series_info = soup.find_all('span', class_='calendrier_episodes')
+    
+    data = []
+    for i, serie in enumerate(series_info):
+        country_img = serie.find_previous('img', alt=True)
+        country = country_img['alt']
+        channel_img = country_img.find_next('img', alt=True)
+        channel = channel_img['alt']
+        series_name = serie.find('a').text
+        episode_details = serie.find('a', class_='liens').text
+        url_episode = serie.find('a', class_='liens')['href']
+        season, episode = episode_details.split('.')
+        data.append((dates[i % len(dates)], country, channel, series_name, season, episode, BASE_URL + url_episode))
+    
+    return sorted(data, key=lambda x: int(''.join(filter(str.isdigit, x[0]))))
 
-# Extraire les informations sur chaque série
-series_info = soup.find_all('span', class_='calendrier_episodes')
 
-data = []
+def save_to_csv(data_sorted):
+    """Sauvegarde les données dans un fichier CSV."""
+    with open('data/files/episodes.csv', 'w', encoding='utf-8') as file:
+        for item in data_sorted:
+            series_name, episode, season, url_episode = item[3], int(item[5]), int(item[4]), item[6]
+            file.write(f"{series_name};{episode};{season};{url_episode} # {simple_type(series_name)} {simple_type(episode)} {simple_type(season)} {simple_type(url_episode)}\n")
 
-for i, serie in enumerate(series_info):
-    country_img = serie.find_previous('img', alt=True)
-    country = country_img['alt']
-
-    channel_img = country_img.find_next('img', alt=True)
-    channel = channel_img['alt']
-
-    series_name = serie.find('a').text
-    episode_details = serie.find('a', class_='liens').text
-    url_episode = serie.find('a', class_='liens')['href']
-
-    # Récupération de la saison et de l'épisode
-    season, episode = episode_details.split('.')
-    data.append((dates[i % len(dates)], country, channel, series_name, season, episode, "https://www.spin-off.fr/" + url_episode))
-
-# Trier les données en fonction des numéros de jour
-data_sorted = sorted(data, key=lambda x: int(''.join(filter(str.isdigit, x[0]))))
-
-# Afficher les données triées
-# for item in data_sorted:
-#     print(item)
 
 def simple_type(value):
+    """Renvoie le type simplifié d'une valeur."""
     t = type(value).__name__
-    if t == 'str':
-        return 'string'
-    if t == 'int':
-        return 'integer'
-    if t == 'float':
-        return 'float'
+    return {
+        'str': 'string',
+        'int': 'integer',
+        'float': 'float'
+    }.get(t, t)
 
 
-# Enregistrez les données dans un fichier CSV
-with open('data/files/episodes.csv', 'w', encoding='utf-8') as file:
+def save_to_sqlite(data_sorted):
+    """Sauvegarde les données dans une base de données SQLite."""
+    conn = sqlite3.connect('data/databases/database.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS episode (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        date TEXT,
+        channel TEXT,
+        country TEXT,
+        series_name TEXT,
+        season INTEGER,
+        episode INTEGER,
+        url_episode TEXT
+    )
+    ''')
     for item in data_sorted:
-        series_name = item[3]
-        episode = int(item[5])
-        season = int(item[4])
-        url_episode = "https://www.spin-off.fr/" + item[6]  # Utilisez l'URL de l'épisode de la liste data_sorted
-        file.write(f"{series_name};{episode};{season};{url_episode} # {simple_type(series_name)} {simple_type(episode)} {simple_type(season)} {simple_type(url_episode)}\n")
+        cursor.execute('''
+        INSERT INTO episode (date, country, channel, series_name, season, episode, url_episode)
+        VALUES (?, ?, ?, ?, ?, ?, ?)
+        ''', item)
+    conn.commit()
+    conn.close()
 
 
-def read_episodes(filename):
-    # Ouvrir le fichier en mode lecture
-    with open(filename, 'r', encoding='utf-8') as file:
-        # Lire toutes les lignes
-        lines = file.readlines()
-
-    # Ignorer la première ligne (l'en-tête)
-    lines = lines[1:]
-
-    # Liste pour stocker les épisodes
-    episodes = []
-
-    for line in lines:
-        # Supprimer les caractères d'espacement en début et fin de ligne
-        line = line.strip()
-
-        # Séparer la ligne en utilisant le séparateur ";"
-        parts = line.split(";")
-
-        # Convertir les données aux types appropriés
-        name = parts[0]
-        episode = int(parts[1])
-        season = int(parts[2])
-        url = parts[3]
-
-        # Ajouter le tuple à la liste
-        episodes.append((name, episode, season, url))
-
-    return episodes
-
-# Test de la fonction
-# episodes_data = read_episodes('data/files/episodes.csv')
-# for episode in episodes_data:
-#     print(episode)
-
-
-# Connexion à la base de données SQLite
-conn = sqlite3.connect('data/databases/database.db')
-cursor = conn.cursor()
-
-# Création de la table 'episode'
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS episode (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    date TEXT,
-    channel TEXT,
-    country TEXT,
-    series_name TEXT,
-    season INTEGER,
-    episode INTEGER,
-    url_episode TEXT
-)
-''')
-
-# Insérer les données dans la table
-for item in data_sorted:
-    date, country, channel, series_name, season, episode_num, url_episode = item
+def save_to_postgresql(data_sorted, database_url):
+    """Sauvegarde les données dans une base de données PostgreSQL."""
+    conn = psycopg2.connect(database_url, sslmode='require')
+    cursor = conn.cursor()
     cursor.execute('''
-    INSERT INTO episode (date, country, channel, series_name, season, episode, url_episode)
-    VALUES (?, ?, ?, ?, ?, ?, ?)
-    ''', (date, country, channel, series_name, season, episode_num, "https://www.spin-off.fr/" + url_episode))
+    CREATE TABLE IF NOT EXISTS episode (
+        id SERIAL PRIMARY KEY,
+        date TEXT,
+        channel TEXT,
+        country TEXT,
+        series_name TEXT,
+        season INTEGER,
+        episode INTEGER,
+        url_episode TEXT
+    )
+    ''')
+    for item in data_sorted:
+        cursor.execute('''
+        INSERT INTO episode (date, country, channel, series_name, season, episode, url_episode)
+        VALUES (%s, %s, %s, %s, %s, %s, %s)
+        ''', item)
+    conn.commit()
+    conn.close()
 
-# Commit des changements et fermeture de la connexion
-conn.commit()
-conn.close()
+
+def main():
+    soup = fetch_web_data(BASE_URL + "calendrier_des_series.html")
+    data_sorted = extract_series_info(soup)
+    save_to_csv(data_sorted)
+    save_to_sqlite(data_sorted)
+    DATABASE_URL = 'postgres://cours_pytho_5421:F7XnoI1TH_KbiiwxghBE@cours-pytho-5421.postgresql.a.osc-fr1.scalingo-dbs.com:33800/cours_pytho_5421?sslmode=prefer'
+    save_to_postgresql(data_sorted, DATABASE_URL)
 
 
-# Remplacez ces valeurs par vos informations d'authentification Scalingo
-DATABASE_URL = 'postgres://cours_pytho_5421:F7XnoI1TH_KbiiwxghBE@cours-pytho-5421.postgresql.a.osc-fr1.scalingo-dbs.com:33800/cours_pytho_5421?sslmode=prefer'
-
-# Connexion à la base de données Scalingo
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-cursor = conn.cursor()
-
-# Création de la table 'episode' (si elle n'existe pas déjà)
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS episode (
-    id SERIAL PRIMARY KEY,
-    date TEXT,
-    channel TEXT,
-    country TEXT,
-    series_name TEXT,
-    season INTEGER,
-    episode INTEGER,
-    url_episode TEXT
-)
-''')
-
-# Insérer les données dans la table
-for item in data_sorted:
-    date, country, channel, series_name, season, episode_num, url_episode = item
-    cursor.execute('''
-    INSERT INTO episode (date, country, channel, series_name, season, episode, url_episode)
-    VALUES (%s, %s, %s, %s, %s, %s, %s)
-    ''', (date, country, channel, series_name, season, episode_num, "https://www.spin-off.fr/" + url_episode))
-
-# Commit des changements et fermeture de la connexion
-conn.commit()
-conn.close()
+if __name__ == "__main__":
+    main()
